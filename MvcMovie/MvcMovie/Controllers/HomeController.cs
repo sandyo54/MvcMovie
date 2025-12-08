1b88e5ce17585a77085d97d0e2b78fa1dd1f5b38
livy <ayolivstop@gmail.com>
2025-12-08 11:22:19 -0500

    added more features

--- MvcMovie/MvcMovie/Controllers/HomeController.cs
+++ MvcMovie/MvcMovie/Controllers/HomeController.cs
@@ -1,32 +1,26 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using MvcMovie.Models;
+using System.Text.Encodings.Web;
 
-namespace MvcMovie.Controllers
+namespace MvcMovie.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    // 
+    // GET: /Home/
+    public IActionResult Index()
     {
-        private readonly ILogger<HomeController> _logger;
+        return View();
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
     }
 }
